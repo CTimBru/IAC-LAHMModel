4ea2d4a505c33aa5a00446030d162eb14260be9e
CTimBru <177049373+CTimBru@users.noreply.github.com>
2025-11-05 17:32:44 -0800

    Data Wrangled

--- data_wrangling.R
+++ data_wrangling.R
@@ -10,8 +10,8 @@ require(terra) # For spatial analysis: https://rspatial.org/pkg/index.html
 require(fs) # File tools: https://www.rdocumentation.org/packages/fs/versions/1.6.6
 #require(ff) # File tools: https://www.rdocumentation.org/packages/ff/versions/1.0-1
 require(geodata) # Grab bioclim data for USA only: https://cran.r-project.org/web/packages/geodata/refman/geodata.html
-require(dplyr) # Easier shapefile manipulation: https://dplyr.tidyverse.org/reference/index.html
-
+require(future.apply) # Multithreading, not great on Windows 11: https://cran.r-project.org/web/packages/future.apply/refman/future.apply.html
+require(tidyverse) # Dataframes: https://www.rdocumentation.org/packages/tidyverse/versions/2.0.0
 
 # Load users working directory
 source("rvar/var.R")
@@ -101,9 +101,11 @@ rm(WHP_file,WHP_files)
 # V) Bioclim BClim: https://www.worldclim.org/data/worldclim21.html
 # USA only bioclim: https://geodata.ucdavis.edu/climate/worldclim/2_1/tiles/iso/USA_wc2.1_30s_bio.tif
 # worldclim_country(var="bio",country="USA",res=0.5,path=".fullDatasets/") path gathered from geodata library
-BioClim_dir <- ".fullDatasets/Bioclim_USA"
+BioClim_dir <- ".fullDatasets/Bioclim"
 
-#File was sourced manually and is in uncompressed format, no further actions
+unzip(".fullDatasets/wc2.1_30s_bio.zip", overwrite=TRUE, exdir=BioClim_dir)
+
+#Delete after Cropping
 
 # VI) LA County assessor data
 # Data: https://data.lacounty.gov/datasets/lacounty::assessor-parcel-data-rolls-2021-present/about
@@ -119,99 +121,190 @@ assessor_dir <- ".fullDatasets/Assessor"
 data_dir <- "LA_Data/"
 
 # I) Study area(s)
-# Get the shapefile names
-Eaton_shape <- list.files(path=Eaton_dir,pattern="*\\.shp$")
-Eaton_shape <- paste0(Eaton_dir,"/",Eaton_shape)
-Palisades_shape <- list.files(path=Palisades_dir,pattern="*\\.shp$")
-Palisades_shape <- paste0(Palisades_dir,"/",Palisades_shape)
+if(file.exists(paste0(data_dir,"Eaton_WF.shp")) && file.exists(paste0(data_dir,"Palisades_WF.shp"))){
+    Eaton_Perimeter <- read_sf(paste0(data_dir,"Eaton_WF.shp"))
+    Palisades_Perimeter <- read_sf(paste0(data_dir,"Palisades_WF.shp"))
+  } else  {
+    # Get the shapefile names
+    Eaton_shape <- list.files(path=Eaton_dir,pattern="*\\.shp$")
+    Eaton_shape <- paste0(Eaton_dir,"/",Eaton_shape)
+    Palisades_shape <- list.files(path=Palisades_dir,pattern="*\\.shp$")
+    Palisades_shape <- paste0(Palisades_dir,"/",Palisades_shape)
+    
+    # Load the shapefiles
+    Eaton_Perimeter <- read_sf(Eaton_shape)
+    Palisades_Perimeter<- read_sf(Palisades_shape)
+    # Reproject to WSG84.
+    # 4326 is WGS84: https://spatialreference.org/ref/epsg/4326/
+    Eaton_Perimeter <- st_transform(Eaton_Perimeter,crs=st_crs(4326))
+    Palisades_Perimeter <- st_transform(Palisades_Perimeter,crs=st_crs(4326))
+    
+    # Output Eaton & Palisades shapefile to data dir
+    st_write(Eaton_Perimeter,paste0(data_dir,"Eaton_WF.shp"))
+    st_write(Palisades_Perimeter,paste0(data_dir,"Palisades_WF.shp"))
+    
+    # var cleanup
+    rm(Eaton_shape,Palisades_shape)
+  }
 
-# Load the shapefiles
-Eaton_Perimeter <- read_sf(Eaton_shape)
-Palisades_Perimeter<- read_sf(Palisades_shape)
-# Reproject to WSG84.
-# 4326 is WGS84: https://spatialreference.org/ref/epsg/4326/
-Eaton_Perimeter <- st_transform(Eaton_Perimeter,crs=st_crs(4326))
-Palisades_Perimeter <- st_transform(Palisades_Perimeter,crs=st_crs(4326))
-
-# Output Eaton & Palisades shapefile to data dir
-st_write(Eaton_Perimeter,paste0(data_dir,"Eaton_WF.shp"))
-st_write(Palisades_Perimeter,paste0(data_dir,"Palisades_WF.shp"))
-
-# var cleanup
-rm(Eaton_shape,Palisades_shape)
-
+if(file.exists(paste0(data_dir,"LA_County.shp"))){
+  LA_County_Perimeter <- read_sf(paste0(data_dir,"LA_County.shp"))
+} else  {
 # II) County Shapefile
-# Get the shapefile names
-CA_county_shape <- list.files(path=California_county_dir,pattern="*\\.shp$")
-CA_county_shape <- paste0(California_county_dir,"/",CA_county_shape)
+  # Get the shapefile names
+  CA_county_shape <- list.files(path=California_county_dir,pattern="*\\.shp$")
+  CA_county_shape <- paste0(California_county_dir,"/",CA_county_shape)
+  
+  # Load the shapefile
+  LA_County_Perimeter <- read_sf(CA_county_shape) %>% dplyr::filter(NAME == "Los Angeles")
+  LA_County_Perimeter <- st_transform(LA_County_Perimeter,crs=st_crs(4326))
+  
+  # Output LA County shapefile to data dir
+  st_write(LA_County_Perimeter,paste0(data_dir,"LA_County.shp"))
 
-# Load the shapefile
-LA_County_Perimeter <- read_sf(CA_county_shape) %>% dplyr::filter(NAME == "Los Angeles")
-LA_County_Perimeter <- st_transform(LA_County_Perimeter,crs=st_crs(4326))
-
-# Output LA County shapefile to data dir
-st_write(LA_County_Perimeter,paste0(data_dir,"LA_County.shp"))
+  # var cleanup
+  rm(CA_county_shape)
+}
 # save bounding box
 LA_County_bbox <- extent(LA_County_Perimeter)
 LA_County_bbox <- st_as_sfc(st_bbox(LA_County_bbox))
 st_crs(LA_County_bbox) = 4326
 
-# var cleanup
-rm(CA_county_shape)
-
 # III) Wildlands-Urban-Interface
-# Get WUI files as a list including dir
-WUI_files <- paste0(WUI_dir,"/",list.files(path=WUI_dir,pattern="*\\.tif$"))
-
-# Make of raster set from all of the separate tifs
-combined_WUI <- vrt(WUI_files)
-
-# Project to WSG84: ESPG4326
-## Warning projecting takes ~20mins. Multithread target
-combined_WUI <- project(combined_WUI,"epsg:4326")
-
-# Crop to LA County
-LA_County_WUI <- crop(combined_WUI,LA_County_Perimeter)
-LA_County_WUI <- mask(combined_WUI,LA_County_Perimeter)
-
-#Crop to less nuts bounding box
-LA_County_WUI <- crop(LA_County_WUI,LA_County_bbox)
-
-# Write the raster
-raster::writeRaster(LA_County_WUI,paste0(data_dir,"WUI.tif"),overwrite=TRUE)
-
-# var cleanup
-rm(WUI_files,combined_WUI)
+if(file.exists(paste0(data_dir,"WUI.tif"))){
+  LA_County_WUI <- raster(paste0(data_dir,"WUI.tif"))
+} else  {
+  # Choose singlethread or multithread
+  # ISSUE: Multithread causes tif artifacts (gaps in data, projection+vrt artifact?)
+  WUI_multithread <- FALSE
+  if (WUI_multithread == TRUE){
+    
+    WUI_files <- paste0(WUI_dir,"/",list.files(path=WUI_dir,pattern="*\\.tif$"))
+    
+    # Define function to run in parallel
+    WGS84_raster_reprojection <- function(i){
+      rasterFile <- WUI_files[i]
+      print(paste("Beginning run:",rasterFile))
+      print(Sys.time())
+      rasterReproj <- projectRaster(raster(rasterFile),crs ="+init=epsg:4326")
+      rasterRename <- gsub("WUI_LA/","WUI_LA/WGS84/",rasterFile)
+      raster::writeRaster(rasterReproj,rasterRename,overwrite=TRUE)
+      print(paste("Ending run:",rasterFile))
+      rm(rasterRename,rasterReproj,rasterFile)
+      print(Sys.time())
+    }
+    
+    #Set plan for cores (use half available). >2 led to memory issues!
+    plan(multisession, workers=2)
+    
+    future_lapply(1:length(WUI_files),FUN=WGS84_raster_reprojection,future.seed=1)
+    
+    #Back to singlecore#Back to singleFUN = core
+    plan(sequential)
+    
+    WUI_files <- paste0(WUI_dir,"/WGS84/",list.files(path=paste0(WUI_dir,"/WGS84"),pattern="*\\.tif$"))
+    
+    combined_WUI <- vrt(WUI_files)
+    LA_County_WUI <- crop(combined_WUI,LA_County_Perimeter)
+    LA_County_WUI <- mask(LA_County_WUI,LA_County_Perimeter)
+    
+    # Write the raster
+    raster::writeRaster(LA_County_WUI,paste0(data_dir,"WUI.tif"),overwrite=TRUE)
+    
+  } else {
+    # Get WUI files as a list including dir
+    WUI_files <- paste0(WUI_dir,"/",list.files(path=WUI_dir,pattern="*\\.tif$"))
+    
+    # Make of raster set from all of the separate tifs
+    combined_WUI <- vrt(WUI_files)
+    
+    # Project to WSG84: ESPG4326
+    ## Warning projecting takes ~20mins. Multithread target
+    combined_WUI <- project(combined_WUI,"epsg:4326")
+    
+    # Crop to LA County
+    LA_County_WUI <- crop(combined_WUI,LA_County_Perimeter)
+    LA_County_WUI <- mask(LA_County_WUI,LA_County_Perimeter)
+    
+    # Write the raster
+    raster::writeRaster(LA_County_WUI,paste0(data_dir,"WUI.tif"),overwrite=TRUE)
+    
+    # var cleanup
+    rm(WUI_files,combined_WUI)
+  }
+}
 
 # IV) WHP
-WHP_CA <- raster(paste0(WHP_dir,"/WHP_CA.tif"))
-
-# Large tif, lets cut this down
-# Convoluted way to get the bbox from WSG84 -> AEA
-AEA_bbox <- st_transform(LA_County_bbox,crs=crs(WHP_CA))
-AEA_df <- cbind(st_sf(geometry=AEA_bbox),do.call(rbind,lapply(AEA_bbox, st_bbox)))
-
-#Crop & Mask WHP_CA before reprojecting
-LA_County_WHP <- crop(WHP_CA,st_bbox(c(xmin=AEA_df[[1]],xmax=AEA_df[[3]],ymin=AEA_df[[2]],ymax=AEA_df[[4]])))
-
-# Reproject to WSG84: EPSG4326
-LA_County_WHP <- projectRaster(LA_County_WHP,crs="epsg:4326")
-
-# Crop to LA County
-LA_County_WHP <- crop(LA_County_WHP,LA_County_Perimeter)
-LA_County_WHP <- mask(LA_County_WHP,LA_County_Perimeter)
-
-# Write the raster
-raster::writeRaster(LA_County_WHP,paste0(data_dir,"WHP.tif"),overwrite=TRUE)
-
-
-# var cleanup
-rm(AEA_bbox,AEA_df,WHP_CA)
-
+if(file.exists(paste0(data_dir,"WHP.tif"))){
+  LA_County_WHP <- raster(paste0(data_dir,"WHP.tif"))
+} else  {
+  # Get WHP file
+  WHP_CA <- raster(paste0(WHP_dir,"/WHP_CA.tif"))
+  
+  # Large tif, lets cut this down
+  # Convoluted way to get the bbox from WSG84 -> AEA
+  AEA_bbox <- st_transform(LA_County_bbox,crs=crs(WHP_CA))
+  AEA_df <- cbind(st_sf(geometry=AEA_bbox),do.call(rbind,lapply(AEA_bbox, st_bbox)))
+  
+  #Crop & Mask WHP_CA before reprojecting
+  LA_County_WHP <- crop(WHP_CA,st_bbox(c(xmin=AEA_df[[1]],xmax=AEA_df[[3]],ymin=AEA_df[[2]],ymax=AEA_df[[4]])))
+  
+  # Reproject to WSG84: EPSG4326
+  LA_County_WHP <- projectRaster(LA_County_WHP,crs="epsg:4326")
+  
+  # Crop to LA County
+  LA_County_WHP <- crop(LA_County_WHP,LA_County_Perimeter)
+  LA_County_WHP <- mask(LA_County_WHP,LA_County_Perimeter)
+  
+  # Write the raster
+  raster::writeRaster(LA_County_WHP,paste0(data_dir,"WHP.tif"),overwrite=TRUE)
+  
+  
+  # var cleanup
+  rm(AEA_bbox,AEA_df,WHP_CA)
+}
 # V) Bioclim
-BioClim_USA <- brick(paste0(BioClim_dir,"/USA_wc2.1_30s_bio.tif"))
-BioClim_USA <- unstack(BioClim_USA)
+# Check the data dir if bioclims are already processed
+if(file.exists(paste0(data_dir,"WUI.tif"))){
+  LA_County_WUI <- raster(paste0(data_dir,"WUI.tif"))
+} else  {
+  bioclim_list <- list.files(path=data_dir,pattern="\\bwc2.1_30s_bio.*")
+  while(length(bioclim_list)<19){
+    # Set list to source dir
+    bioclim_list <- list.files(path=BioClim_dir,pattern="\\bwc2.1_30s_bio.*")
+    for(bioclim in bioclim_list){
+      #Read in bioclim layers
+      bioclim_layer <- raster(paste0(BioClim_dir,"/",bioclim))
+      #printing datum, if projection is not EPSG4326 | WSG84 then recode for reprojection!
+      print(crs(bioclim_layer, asText = TRUE))
+      #n.b. suggested to build a bounding box in that co-ords & then crop before reprojection, due to massive files
+      if(grepl("WGS84", crs(bioclim_layer, asText = TRUE))){
+        # Crop & Mask to LA
+        bioclim_layer <- crop(bioclim_layer,LA_County_Perimeter)
+        bioclim_layer <- mask(bioclim_layer,LA_County_Perimeter)
+        # Crop bioclim to bbox around LA County
+        # Output to LA dir
+        raster::writeRaster(bioclim_layer,paste0(data_dir,bioclim),overwrite=TRUE)
+      } else {print('Coordinate system requires reprojection to continue!')}
+    }
+  }
+}
 
+#VI) Assessor
+
+#We are just interested in location & building age
+
+if(file.exists(paste0(data_dir,"ParcelData_2021.csv"))){
+  LA_County_Parcels <- read_csv(paste0(data_dir,"ParcelData_2021.csv"))
+} else  {
+  LA_County_Parcels <- read_csv(paste0(assessor_dir,"/Parcel_Data_2021_Table_8468414499611436475.csv"))
+  #Zip Code...1: 9 digit zip
+  #Property Use Type: SFR- Single Family Residence, NA, VAC- Vacant, C/I- Commercial/Industrial, R-I- ?Multi-family?, CND- Condominium
+  #Year Built: Year 50%+ of the original structure was complete
+  #Effective Year: Last year of any 'substantial' new construction or major rehabilitation.
+  LA_County_Parcels <- LA_County_Parcels[c('Zip Code...1','Property Use Type','Year Built','Effective Year','Location Latitude','Location Longitude')] %>% as_tibble(.name_repair = 'universal')
+  write_csv(LA_County_Parcels,paste0(".fullDatasets/ParcelData_2021.csv"))
+}
 
 #Cali Bounding Box + floor() ceiling()
 #-124.41060660766607,32.5342307609976,-114.13445790587905,42.00965914828148: https://observablehq.com/@rdmurphy/u-s-state-bounding-boxes
